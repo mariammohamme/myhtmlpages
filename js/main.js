c20b60812620d6b955e2635e02ee850c583c1c34
mariammohamme <mariam.mohammed942@hotmail.com>
2020-12-26 18:26:44 +0200

    lab6_b

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var nam= prompt("please enter the identyty");
+if (nam === "mariam")
+{
+    alert("hi mariam." + nam )
+}
+else
+{
+    alert("wronge name")
+}
+while(nam !=='father')
+{
+    nam = prompt('the name of father shoul put');
+}
+var number = prompt ('put your age please!');
+while (number !== '20')
+{
+    alert("welcome")
+}
