113ac77975d27c69fabeaa6ce69286ee11794112
mariammohamme <mariam.mohammed942@hotmail.com>
2020-12-26 19:41:10 +0200

    lab_6

--- js/main.js
+++ js/main.js
@@ -7,12 +7,3 @@ else
 {
     alert("wronge name")
 }
-while(user !=='father')
-{
-    nam = prompt('the name of father shoul put');
-}
-var number = prompt ('put your age please!');
-while (number !== '20')
-{
-    alert("welcome")
-}
