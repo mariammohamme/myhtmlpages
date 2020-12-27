5dbe87082fbd06f09f61d1863e8f68b5d400a9fb
mariammohamme <mariam.mohammed942@hotmail.com>
2020-12-27 15:10:54 +0200

    lab-8

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
-var user= prompt("please enter the identyty");
-if ( user === "mariam")
-{
-    alert("hi mariam." + user )
+prompt('hello, what is Ur name plz');
+var user = prompt("how many chooclet you have?");
+if (user == 0) {
+    document.write('<img src="chooclet.jpg">');
 }
-else
-{
-    alert("wronge name")
+else if (user == 1) {
+    document.write('<img src="chooclet.jpg">');
+    document.write('<img src="chooclet.jpg">');
+}else if (user == 2) {
+    document.write('<img src="chooclet.jpg">');
+    document.write('<img src="chooclet.jpg">');
+    document.write('<img src="chooclet.jpg">');
 }
+
+/* for ( user = 0; user < 3; i++) {
+   
+  
+    if (user == 0 )
+ { document.write('<img src="chooclet.jpg">');} 
+   else if (user == 1) {
+   document.write('<img src="chooclet.jpg">');
+   document.write('<img src="chooclet.jpg">');
+alert("wronge name");
+   }
+} 
+*/
